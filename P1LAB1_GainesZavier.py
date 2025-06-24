dd8549a8058ea230ce21dcc044fc34e1b7dfdeae
UncleDrive <gainesz3709@student.faytechcc.edu>
2025-06-23 23:50:30 -0400

    Add files via upload

---
+++ P1LAB1_GainesZavier.py
@@ -0,0 +1,7 @@
+# Zavier Gaines
+# June 16 2025
+# P1LAB1
+# Greets us ask for our names too its like a introduction in some way
+first_name=input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
