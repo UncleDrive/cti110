f445141898bdad8eb344ce841bdf0f16460ff89e
UncleDrive <gainesz3709@student.faytechcc.edu>
2025-06-30 22:48:18 -0400

    Add files via upload

---
+++ P2HW1_GainesZavier.py
@@ -0,0 +1,24 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================ RESTART: D:\Class Phython\P1HW2_GainesZavier.py ===============
+This program calculates and displays travel expenses
+
+Enter Budget: 2000
+
+Enter your travel destination: new york city
+
+How much do you think you will spend on gas? 450
+
+Approximately how much will you need for accomidation/hotel? 600
+
+Last how much do you need for food? 250
+
+------------Travel Expenses------------
+Location:            new york city
+Initial Budget:     $ 2000.00
+Fuel:               $ 450.00
+Accomidation:       $ 600.00
+Food:               $ 250.00
+---------------------------------------
+Remaining Balance:  $ 700.00
